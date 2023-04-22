b042e0d69dee5f0313877b9fe9f39a3b3c0dec56
long2ice <long2ice@gmail.com>
2023-04-22 22:56:00 +0800

    fix: admin init

--- src/api/init.ts
+++ src/api/init.ts
@@ -1,7 +1,7 @@
 import http from '@/axios'
 
 export async function initAdmin(email: string, password: string, nickname: string): Promise<void> {
-  await http.post('/admin/init', {
+  await http.post('/init/admin', {
     email,
     password,
     nickname
